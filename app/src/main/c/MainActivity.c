70fac2e27dfc72785cc0fc0241cdbd4e8e30bda8
agayen <abhijit@peartree.to>
2024-04-18 13:45:42 +0530

    console.log in logcat & html css js UI

--- app/src/main/c/MainActivity.c
+++ app/src/main/c/MainActivity.c
@@ -1,13 +1,52 @@
+#include <android/log.h>
 #include <assert.h>
 #include <bare.h>
 #include <jni.h>
+#include <unistd.h>
 #include <uv.h>
 
 #include "MainActivity.bundle.h"
 
+static int pfd[2];
+static pthread_t thr;
+static const char *tag = "myapp";
+
+static void *thread_func(void*)
+{
+  ssize_t rdsz;
+  char buf[128];
+  while((rdsz = read(pfd[0], buf, sizeof buf - 1)) > 0) {
+    if(buf[rdsz - 1] == '\n') --rdsz;
+    buf[rdsz] = 0;  /* add null-terminator */
+    __android_log_write(ANDROID_LOG_DEBUG, tag, buf);
+  }
+  return 0;
+}
+
+int start_logger(const char *app_name)
+{
+  tag = app_name;
+
+  /* make stdout line-buffered and stderr unbuffered */
+  setvbuf(stdout, 0, _IOLBF, 0);
+  setvbuf(stderr, 0, _IONBF, 0);
+
+  /* create the pipe and redirect stdout and stderr */
+  pipe(pfd);
+  dup2(pfd[1], 1);
+  dup2(pfd[1], 2);
+
+  /* spawn the logging thread */
+  if(pthread_create(&thr, 0, thread_func, 0) == -1)
+    return -1;
+  pthread_detach(thr);
+  return 0;
+}
+
 JNIEXPORT void JNICALL
 Java_com_holepunch_bare_MainActivity_init (JNIEnv *env, jobject self) {
   int err;
+  start_logger("my_bare_app");
 
   int argc = 0;
   char **argv = NULL;
--- app/src/main/java/MainActivity.java
+++ app/src/main/java/MainActivity.java
@@ -1,18 +1,31 @@
 package com.holepunch.bare;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 
 public class MainActivity extends Activity {
   static {
     System.loadLibrary("bare_android_shared");
   }
 
+  private WebView webView;
+
   public native void init();
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.mainactivity);
+    // Initialize WebView
+    webView = findViewById(R.id.webView);
+    WebSettings webSettings = webView.getSettings();
+    webSettings.setJavaScriptEnabled(true); // Enable JavaScript
+    webView.loadUrl("file:///android_asset/index.html"); // Load HTML file from assets
 
     init();
   }
